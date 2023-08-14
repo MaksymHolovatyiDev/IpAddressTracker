02630fb76fafe2d82233119fbdecbdae467abfbc
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-14 15:13:46 +0300

    Add map

---
+++ src/components/Map/Map.styled.ts
@@ -0,0 +1,10 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+    map: {
+      flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+});
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,35 @@
-// import { LatLng, LeafletView } from 'react-native-leaflet-view';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgXml } from 'react-native-svg';
 
-// export default function Map() {
-//   return <LeafletView />;
-// }
+import { locationImg } from '@assets/icons/svg';
+import { styles } from './Map.styled';
+import { useMain } from 'components/MainProvider/MainProvider';
+
+export default function Map() {
+  const {
+    MainUserData: { lat, lng, location },
+  } = useMain();
+
+  return (
+    <MapView
+      style={styles.map}
+      mapType="standard"
+      region={{
+        latitude: lat ?? 0,
+        longitude: lng ?? 0,
+        latitudeDelta: 0.0992,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: lat,
+          longitude: lng,
+        }}
+        description={location}
+      >
+        <SvgXml xml={locationImg} style={{ height: 35, width: 35 }} />
+      </Marker>
+    </MapView>
+  );
+}
